0dc9e7f6a79057d1371259b9b82ac8f06131ddfe
k2pbac <krisbachan123@hotmail.com>
2021-08-25 20:52:09 -0400

    add client and server files

---
+++ client.js
@@ -0,0 +1,18 @@
+const net = require("net");
+
+const conn = net.createConnection({ 
+  host: 'localhost', // change to IP address of computer or ngrok host if tunneling
+  port: 3000 // or change to the ngrok port if tunneling
+});
+// client.js
+conn.on('data', (data) => {
+  console.log('Server says: ', data);
+});
+
+// client.js
+
+conn.on('connect', () => {
+  conn.write('Hello from client!');
+});
+
+conn.setEncoding('utf8'); // interpret data as text
